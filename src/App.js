6e8025f7c1e7452f1686b36e5a7753314a057ff3
BaoQuocZero <130838918+BaoQuocZero@users.noreply.github.com>
2024-02-02 10:42:13 +0700

    Render "Dữ Liệu của Biến" với React Hook

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
+
+  let name = "Bao";
+  let obj = { name: "Quoc", god: "Trai Dat" }
+  let link = "https://www.youtube.com/watch?v=Y9gTouaZJ5s&list=PLncHg6Kn2JT4xzJyhXfmJ53dzwVbq-S_E&index=10"
   return (
     <div className="App">
+      {console.log("Check obj: ", obj)}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello World</h1>
+        <h1>Hello World by {name}</h1>
+        <h1>Obj = {obj.name} god of {obj.god}</h1>
+        <a href={link} target='_blank'>Video React</a>
+        <p>{JSON.stringify(obj)}</p>
       </header>
     </div>
   );
