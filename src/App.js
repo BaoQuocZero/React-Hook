1072a13c9f333786a8f17547fe5255dd4528581b
BaoQuocZero <130838918+BaoQuocZero@users.noreply.github.com>
2024-02-07 16:05:54 +0700

    Components với React Hook

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Nav from './views/Nav/Nav';
+
 const App = () => {
 
   let name = "Bao";
@@ -8,7 +10,7 @@ const App = () => {
   let link = "https://www.youtube.com/watch?v=Y9gTouaZJ5s&list=PLncHg6Kn2JT4xzJyhXfmJ53dzwVbq-S_E&index=10"
   return (
     <div className="App">
-      {console.log("Check obj: ", obj)}
+      <Nav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello World by {name}</h1>
